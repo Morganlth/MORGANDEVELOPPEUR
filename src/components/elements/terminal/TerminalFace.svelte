910fbe6d3460a9e589cb5558ec76b62654eb03c7
Morgan <lethuaut.morgan@gmail.com>
2023-10-31 17:10:50 +0100

    commit 31/10/23

--- src/components/elements/terminal/TerminalFace.svelte
+++ src/components/elements/terminal/TerminalFace.svelte
@@ -139,7 +139,7 @@
         output_setVars()
 
         setTimeout(() => {
-            output_c$Log(document.querySelector('#home .title .element .icon').offsetWidth)
+            output_c$Log('' + document.querySelector('#home .title .element .icon').offsetWidth)
         }, 2000)
     }
 
