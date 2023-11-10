6e5297443e458a718652be78bd06a6f9bb3f06e7
Morgan <lethuaut.morgan@gmail.com>
2023-11-10 09:16:04 +0100

    commit 09/11/23

--- src/main/pages/page/projects/cards/Card.svelte
+++ src/main/pages/page/projects/cards/Card.svelte
@@ -370,13 +370,6 @@ on:out={tag_eOut}
     // --*
     async function card_e$MouseMove(clientX, clientY) { card_updateTranslate(clientX, clientY) }
 
-    async function card_e$TouchMove(clientX, clientY)
-    {
-        card_updateTranslate(clientX, clientY)
-
-        tag_updateTranslate(clientX, clientY)
-    }
-
     function card_e$MouseUp()
     {
         if (+new Date() < card_LAST + CARD_DELAY) card_eClick()
@@ -384,6 +377,13 @@ on:out={tag_eOut}
         card_destroyEvents()
         
         card_GRABBING = false
+    }
+
+    async function card_e$TouchMove(clientX, clientY)
+    {
+        card_updateTranslate(clientX, clientY)
+
+        tag_updateTranslate(clientX, clientY)
     }
 
     async function card_e$Resize()
