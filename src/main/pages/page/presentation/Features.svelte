c678546e9cad45f1469f8af90ed3ed0ae1646834
Morgan <lethuaut.morgan@gmail.com>
2023-11-17 09:05:45 +0100

    commit 16/11/23

--- src/main/pages/page/presentation/Features.svelte
+++ src/main/pages/page/presentation/Features.svelte
@@ -390,7 +390,7 @@ lang="scss"
         {
             #{--cell-border-color}: $primary;
 
-            @include utils.placement(absolute, $top: 20%, $right: app.$gap-inline);
+            @include utils.placement(absolute, $top: 18%, $right: app.$gap-inline);
 
             transform: translateY(-50%);
         }
