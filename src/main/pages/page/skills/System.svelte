ec22e9f26e5b7318bd54ea8d65b68c75972fcb71
Morgan <lethuaut.morgan@gmail.com>
2023-12-01 18:30:32 +0100

    commit 01/12/23

--- src/main/pages/page/skills/System.svelte
+++ src/main/pages/page/skills/System.svelte
@@ -27,7 +27,7 @@ context="module"
 <!-- #|-HTML-| -->
 
 <div
-class="system {$APP_$USER_AGENT}"
+class="system"
 style:--system-perspective="{gravityarea_ORBIT_RADIUS * 2}px"
 style:--system-r-x={system_ROTATE_X}
 style:--system-r-y={system_ROTATE_Y}
@@ -153,7 +153,6 @@ style:--system-r-y={system_ROTATE_Y}
     // --SVELTE
 
     // --CONTEXTS
-    const APP_$USER_AGENT = APP.app_$USER_AGENT
 
     // --OUTSIDE
 
